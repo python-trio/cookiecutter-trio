c899fde973ae7bfe31a1c4ad807f2de6b5f04434
Nathaniel J. Smith <njs@pobox.com>
2018-09-11 18:07:34 -0700

    Python 3.7 is out!
    Add 3.7 to the CI, and to the generated projects, and clean up the
    "minimum supported python version" logic a bit.

--- tests/test_stuff.py
+++ tests/test_stuff.py
@@ -1,2 +1,14 @@
+import subprocess
+import cookiecutter
+
+# make venv
+# inside the new venv, install test requirements and check pytest <project
+# name> works
+
+# make venv
+# inside the new venv, install ci/rtd-requirements.txt, and check sphinx-build
+# works
+# oh and towncrier too
+
 def test_stuff():
     print("woo")
