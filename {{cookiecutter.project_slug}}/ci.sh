07deaa36bcdd99d4575804184ad86e5a0e3dc88c
Kyle Altendorf <sda@fstab.net>
2020-09-03 10:27:00 -0400

    Add missing installation of pep517 dependency

--- {{cookiecutter.project_slug}}/ci.sh
+++ {{cookiecutter.project_slug}}/ci.sh
@@ -73,7 +73,7 @@ fi
 
 python -c "import sys, struct, ssl; print('#' * 70); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO); print('#' * 70)"
 
-python -m pip install -U pip setuptools wheel
+python -m pip install -U pip setuptools wheel pep517
 python -m pip --version
 
 python -m pep517.build --source --out-dir dist/ .
